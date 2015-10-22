fc935a6dddd4b3c5236f331eb0692e38084f8b66
ZENGXH <4650xh@gmail.com>
2015-10-22 18:37:45 +0200

    recover pinv..

--- project1/ridgeRegression.m
+++ project1/ridgeRegression.m
@@ -1,5 +1,5 @@
 function beta = ridgeRegression(y, tX, lambda)
 lambdaI = lambda * eye(length(tX' * tX));
 lambdaI(1,1) = 0;
-beta = inv(tX' * tX + lambdaI) * tX' * y;
+beta = pinv(tX' * tX + lambdaI) * tX' * y;
 end
