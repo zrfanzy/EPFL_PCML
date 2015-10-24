430349515cbb06750ab5fe2fa88d4c4ed63d43d4
ZENGXH <4650xh@gmail.com>
2015-10-24 18:20:35 +0200

    48 is good:)

--- project1/help_function/split_feature48.m
+++ project1/help_function/split_feature48.m
@@ -1,6 +1,6 @@
-function col12 = split_feature12(X)
+function col12 = split_feature48(X)
 col12 = zeros(size(X,1),2);
-index1 = find(X<=14);
+index1 = find(X>14);
 col12(index1,1) = X(index1);
-index2 = find(X>14);
+index2 = find(X<=14);
 col12(index2,2) = X(index2);
