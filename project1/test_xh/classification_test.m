2b5e82ec2e17ff6ade96cc0c1b4a7c7070ac6b70
ZENGXH <4650xh@gmail.com>
2015-10-26 19:36:15 +0100

    find a typo

--- project1/test_xh/classification_test.m
+++ project1/test_xh/classification_test.m
@@ -1,11 +1,11 @@
 clear all
 % ridgeRegression
 load('Shanghai_classification.mat');
-% X = normalizeFeature(X_train);
-X = X_train;
+X = normalizeFeature(X_train);
+%X = X_train;
 % y = normalizeFeature(y_train);
 y = normalization_claasi(y_train);
-X = [X expandx(X)];
+X = [X expandx(X_train)];
 
 prop = 0.8;
 [XTr, yTr, XTe, yTe] = split(y, X, prop);
@@ -23,7 +23,7 @@ end
 % lambda values found for each degree
 % lambda = logspace(-5,15 ,20);
 lambda = logspace(-2,6,100);
-lambda = [0,lambda];
+% lambda = 0;
     for i = 1:length(lambda)
     % K-fold cross validation for each lambda
         for k = 1:K
@@ -45,7 +45,7 @@ lambda = [0,lambda];
             tXTe_cv = [ones(length(yTe_cv), 1) XTe_cv]; 
 
             % ridge regression    
-            alpha = 0.005;
+            alpha = 0.08;
             beta = penLogisticRegression(yTr_cv,tXTr_cv,alpha,lambda(i));
 
             mseTrSub(k) = computeCost(yTr_cv, tXTr_cv, beta); 
@@ -61,8 +61,8 @@ lambda = [0,lambda];
     end % end of runing for different lambda
     
 [numb,index_best_lambda] = min(rmseTe_lamb);
-
-beta = penLogisticRegression(yTr_cv,tXTr_cv,alpha,lambda(index_best_lambda));
+% beta = logisticRegression(yTr,tXTr,alpha);
+ beta = penLogisticRegression(yTr,tXTr,alpha,lambda(index_best_lambda));
 mseTr = computeCost(yTr, tXTr, beta);
 mseTe = computeCost(yTe, tXTe, beta);
 fprintf('tr %.4f te %.4f and lambda%.3f \n',mseTr,mseTe,lambda(index_best_lambda));
