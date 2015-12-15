f079f2d4d49318f53bc9f4a22b2533a46277fcee
Ruofan Zhou <zrfanzy@gmail.com>
2015-12-15 16:21:22 +0100

    .

--- project2/visualhog.m
+++
@@ -1,9 +0,0 @@
-function hog = visualhog(p)
-
-load('../train.mat');
-x = train.X_hog(p,:);
-x = reshape(x,13,13,32);
-x = x(:,:,1:31);
-imhog=vl_hog('render',x,'verbose');
-clf ; imagesc(imhog) ; colormap gray ;
-hog = imhog;
